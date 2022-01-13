83822c7b04e269ed1d68afdf6413d1c55bf272c2
tahczeban <90135381+tahczeban@users.noreply.github.com>
2022-01-13 13:59:44 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
+import scraping
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+
 app = Flask(__name__)
-# Use flask_pymongo to set up mongo connection
+
+# Use flask_PyMongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
 @app.route("/")
 def index():
-   mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
-   @app.route("/scrape")
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
-    return redirect('/', code=302) 
-    #update_one(query_parameter, {"$set": data}, options)
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
-    return redirect('/', code=302)
-    if __name__ == "__main__":
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+    
+if __name__ == "__main__":
     app.run()
